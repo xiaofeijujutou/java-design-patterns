2da4bebd9e4727ef427c6c2ee1b41fd801a14163
xiaofeijujutou <1305392100@qq.com>
2024-03-02 12:02:12 +0800

    init

---
+++ designpatterns/src/test/java/com/xiaofei/designpatterns/DesignpatternsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiaofei.designpatterns;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesignpatternsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
